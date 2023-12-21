42d8cdb6b72e3e8587bded9ec20825c2b229ea89
tanesan0930 <ryo.hirata@mmmcorp.co.jp>
2023-12-21 15:55:07 +0900

    add

--- hsd.go
+++ hsd.go
@@ -1,9 +1,12 @@
+// Package hsd provides a function to calculate the Hamming distance between two strings.
 package hsd
 
+// StringDistance returns the Hamming distance between two strings.
 func StringDistance(lhs, rhs string) int {
 	return Distance([]rune(lhs), []rune(rhs))
 }
 
+// Distance returns the Hamming distance between two rune slices.
 func Distance(a, b []rune) int {
 	dist := 0
 	if len(a) != len(b) {
